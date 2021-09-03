6baee130ec7b1fe1618aa57875b14110c6a281a7
Alex Aguirre <aguirre_alex@rocketmail.com>
2021-09-02 23:47:37 -0500

    commit tree e95c653c5f47b4a2832c6f614d30206e3a1e1ef3

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = () => 2000 / 100;
+divide();
+
+const square = value => value ** 2;
+let x = 2;
+square(x);
+
+const add = (value1, value2) => value1 + value2;
+let a = 3;
+let b = 4;
+add(a, b);
